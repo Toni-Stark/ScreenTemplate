54e354d4ddc136526062ccc1e87328ae6d37bcbc
lihao <15502354225@163.com>
2022-07-28 17:54:18 +0800

    工信

--- android/app/src/main/java/com/datascreen/MainActivity.java
+++ android/app/src/main/java/com/datascreen/MainActivity.java
@@ -1,4 +1,4 @@
-package com.huangsi;
+package com.gongxin;
 import android.os.Bundle;
 import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivity;
--- android/app/src/main/java/com/datascreen/MainApplication.java
+++ android/app/src/main/java/com/datascreen/MainApplication.java
@@ -1,4 +1,4 @@
-package com.huangsi;
+package com.gongxin;
 
 import android.app.Application;
 import android.content.Context;
@@ -66,7 +66,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.huangsi.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.gongxin.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
